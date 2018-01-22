32d64fc03084468b811f3a63548e28ce2b9bd399
Sunny Islam <sunnyi@inventas.no>
2018-01-22 12:19:28 +0100

    Added start compress button on port 4000

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var socket = require('socket.io');
 fs = require('fs');
 
 
@@ -30,10 +31,13 @@ function update_bin(original, incoming) {
 
 // Check if bin is compressing
 function compress_cycle(original, incoming) {
-    if (original.compress == 1 && parseFloat(incoming.compress) == 0 ) {
-
+    if (original.compress == 1 && parseFloat(incoming.compress) == 0) {
+        original.start_compress = 0;
         // Add compress cycle
         original.cycles += 1;
+
+
+        // Resert start_compress
 
         // Add new timestamp
         var timestamp = new Date();
@@ -41,24 +45,21 @@ function compress_cycle(original, incoming) {
     }
 }
 
-// Create server
+// Create back-end server
 var app = express();
 
-/*
-app.get('/get/', function (req, res) {
-    let json = JSON.stringify(m, null, 4);
-    res.send({compress: m.bin1.compress});
-});
-*/
-
 app.get('/send/', function (req, res) {
-    let json = JSON.stringify(m, null, 4);
-    res.send({ compress: m.bin1.compress });
+    // Read json
+    let m = JSON.parse(fs.readFileSync('bins.json').toString());
+
     // Check if compress is changed from active to inactive
     compress_cycle(m.bin1, req.query);
     // Update data based on query string
     update_bin(m.bin1, req.query);
 
+    // Send updated start_compress
+    res.send({ start_compress: m.bin1.start_compress });
+    let json = JSON.stringify(m, null, 4);
     // Write beautified json
     fs.writeFile('bins.json', json, 'utf8');
 });
@@ -70,3 +71,35 @@ app.get('/data/', function (req, res) {
 });
 
 app.listen(3000);
+
+
+//////////////////////
+
+// Create front-end server
+var view = express();
+
+// Listening
+var server = view.listen(4000, function () {
+    console.log('listenting to port 4000');
+});
+
+// Add static folder
+view.use(express.static('public'));
+
+// Socket
+var io = socket(server);
+
+io.on('connection', function (socket) {
+    socket.on('loaded', function () {
+        console.log('has loaded');
+    });
+    socket.on('send', function () {
+
+        let m = JSON.parse(fs.readFileSync('bins.json').toString());
+        m.bin1.start_compress = 1;
+
+        let json = JSON.stringify(m, null, 4);
+        fs.writeFile('bins.json', json, 'utf8');
+    });
+
+});
